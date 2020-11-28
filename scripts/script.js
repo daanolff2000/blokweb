b3118a36ab1c1142516bd3d6de8136841adcc485
daanolff2000 <74178103+daanolff2000@users.noreply.github.com>
2020-11-28 21:38:49 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 // JavaScript Document
 var hamburgerKnop = document.querySelector('#hamburgerknop');
 var navigatie = document.querySelector('#navigatie');
+var section = document.querySelector('section');
 
 hamburgerKnop.addEventListener('click', uitklappen);
 
 function uitklappen() {
-	navigatie.classList.toggle('actief');
+	if (navigatie.style.opacity === '1'){
+		navigatie.style.opacity = '0';
+	} else {
+		navigatie.style.opacity = '1';
+	}
+	navigatie.classList.toggle ('actief');
+	section.classList.toggle ('donker');
 }
+
