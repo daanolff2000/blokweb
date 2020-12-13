09db59c8fd934367dbc473a2bf6e0bbbce2e7fa6
daanolff2000 <74178103+daanolff2000@users.noreply.github.com>
2020-12-13 21:32:57 +0100

    video

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,30 @@
 // JavaScript Document
 var hamburger = document.querySelector('#hamburger');
 
+var poster = document.querySelector('.poster');
+var video = document.querySelector('video');
+var timestamp = document.querySelector('.timestamp-container');
+
 function maakActief() {
 	document.body.classList.toggle ('actief');
 }
 
+function playVideo() {
+	poster.classList.add('hidden');
+	video.classList.remove('hidden');
+	video.play();
+	video.loop = true;
+	timestamp.classList.add('hidden');
+}
+
+function videoReset() {
+	poster.classList.remove('hidden');
+	video.classList.add('hidden');
+	video.pause();
+	video.currentTime = 0;
+	timestamp.classList.remove('hidden');
+}
+
 hamburger.addEventListener('click', maakActief);
+poster.addEventListener('mouseover', playVideo);
+video.addEventListener('mouseout', videoReset);
